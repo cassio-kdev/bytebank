92e84189129e90508686f941cd118c79e923f5ac
Cássio Alves Silva <cassio.alves09@gmail.com>
2021-06-28 19:12:55 -0300

    Exibir valor transferido

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,17 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
   styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent {
 
-  valor: number;
-  destino: number;
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
 
   transferir(){
+    const objEmitter = { valor: this.valor, destino: this.destino}
+
     console.log('Solicitada novo transferênciaasdfasds', this.valor);
+    this.aoTransferir.emit(objEmitter);
+    this.limparCampos();
   }
 
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
 }
